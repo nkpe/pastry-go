188c87649c83e9cd7104062d7ba2a6dbb99801c9
nkpe <naomi.pearse@gmail.com>
2022-09-24 19:23:25 +0100

    header and footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/static/Static.css';
 import { Outlet } from 'react-router-dom';
 
+import Header from './components/static/Header';
+import Footer from './components/static/Footer';
+
 function App() {
-  
+
   return (
     <div className="App">
-      <Outlet />
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
