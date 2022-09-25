b353b964ec6111c238c09a9438422622d31502ca
Tom Robinson <tomrobin@cisco.com>
2022-09-25 12:41:33 +0100

    Add second player

--- src/components/pages/game/GameBoard.tsx
+++ src/components/pages/game/GameBoard.tsx
@@ -51,8 +51,8 @@ export default function GameBoard() {
         return 3;
     };
 
-    const [playerId, setPlayerId] = useState(generatePlayerId());
-    const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
+    const [playerPosition, setPlayerPosition] = useState({x: 0, y: 0});
+    const [playerTwoPosition, setPlayerTwoPosition] = useState({x: 30, y: 0});
 
     let squares: any = [];
 
@@ -61,30 +61,46 @@ export default function GameBoard() {
         squares.push(<BoardSquare key={i} id={numbers[i]}/>);
     }
 
-    const playerMove = () => {
-        if (playerPosition.x < (75*11) && playerPosition.y < (75*5)) {
-            setPlayerPosition({x: playerPosition.x+75, y: 0});
-        } else if (playerPosition.y < (75*5)) {
-            setPlayerPosition({x:playerPosition.x, y: playerPosition.y+75});
+    const playerOnePosFac = 75; 
+    const playerOneMove = () => {
+        if (playerPosition.x < (playerOnePosFac*11) && playerPosition.y < (playerOnePosFac*5)) {
+            setPlayerPosition({x: playerPosition.x+playerOnePosFac, y: 0});
+        } else if (playerPosition.y < (playerOnePosFac*5)) {
+            setPlayerPosition({x:playerPosition.x, y: playerPosition.y+playerOnePosFac});
         } else if (playerPosition.x > 0) {
-            setPlayerPosition({x:playerPosition.x - 75, y: playerPosition.y});
+            setPlayerPosition({x:playerPosition.x - playerOnePosFac, y: playerPosition.y});
         } else {
-            setPlayerPosition({x:playerPosition.x, y: playerPosition.y-75});
+            setPlayerPosition({x:playerPosition.x, y: playerPosition.y-playerOnePosFac});
         }
     }
 
-    console.log("MOVE PLAYER: " + JSON.stringify(playerPosition));
+    const playerTwoPosFac = 75;
+    const playerTwoMove = () => {
+        if (playerTwoPosition.x < (playerTwoPosFac*11) && playerTwoPosition.y < (playerTwoPosFac*5)) {
+            setPlayerTwoPosition({x: playerTwoPosition.x+playerTwoPosFac, y: 0});
+        } else if (playerTwoPosition.y < (playerTwoPosFac*5)) {
+            setPlayerTwoPosition({x:playerTwoPosition.x, y: playerTwoPosition.y+playerTwoPosFac});
+        } else if (playerTwoPosition.x > 0) {
+            setPlayerTwoPosition({x:playerTwoPosition.x - playerTwoPosFac, y: playerTwoPosition.y});
+        } else {
+            setPlayerTwoPosition({x:playerTwoPosition.x, y: playerTwoPosition.y-playerTwoPosFac});
+        }
+    }
+
+    console.log("MOVE PLAYER1: " + JSON.stringify(playerPosition));
+    console.log("MOVE PLAYER2: " + JSON.stringify(playerTwoPosition));
 
     return (
         <div id="game-board">
-            <PlayerIcons playerId={playerId} playerPosition={playerPosition} />
+            <PlayerIcons playerId={1} playerPosition={playerPosition} colour="red"/>
+            <PlayerIcons playerId={2} playerPosition={playerTwoPosition} colour="blue"/>
             {squares}
             <GameInner />
-            <button onClick={playerMove}>Click Me!!</button>
+            <button onClick={playerOneMove}>Move Player 1</button>
+            <button onClick={playerTwoMove}>Move Player 2</button>
             <div id="player-cards-wrapper">
                 <GameCardsDeck />
             </div>
-
         </div>
     )
 };
