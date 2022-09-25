1a03a31dd4693a02fb0cee7cae07411ed5d71098
nkpe <naomi.pearse@gmail.com>
2022-09-25 12:39:34 +0100

    game cards made

--- src/components/pages/game/GameBoard.tsx
+++ src/components/pages/game/GameBoard.tsx
@@ -1,17 +1,46 @@
 import React, { useState } from 'react';
 import "./GameBoard.css";
 import PlayerIcons from "../game/PlayerIcons";
-import GameCardsDeck from './GameCards';
-function GameCardPlaceholder() {
+import {GameCardsDeck, GameCardComp} from './GameCards';
+
+import croissantImg from '../../../imgs/cards/croissant-512x512.png';
+import bdayCakeImg from '../../../imgs/cards/birthday-cake-512x512.png';
+import cherryPieImg from '../../../imgs/cards/cherry-pie-512x512.png';
+import pretzelImg from '../../../imgs/cards/pretzel-512x512.png';
+
+function GameCardPlaceholder({ card}: any) {
+
     return (
-        <div className="game-card-placeholder"></div>
+        <div className="game-card-placeholder">
+            {card}
+        </div>
     )
 }
 
-function BoardSquare({id}: any) {
+function BoardSquare({ id }: any) {
     return (
         <div id={id} className="board-game-square">
             <h2>{id}</h2>
+        </div>
+    )
+}
+
+function GameInner() {
+
+    const croissant = <GameCardComp imageSrc={croissantImg} cardValue={1} cardId="1a" cardName="Croissant" />;
+   
+    const birthdayCake = <GameCardComp imageSrc={bdayCakeImg} cardValue={10} cardId="1b" cardName="Birthday Cake" />;
+   
+    const cherryPie = <GameCardComp imageSrc={cherryPieImg} cardValue={5} cardId="1c" cardName="Cherry Pie" />
+    
+    const pretzel = <GameCardComp imageSrc={pretzelImg} cardValue={3} cardId="1d" cardName="Pretzel" />
+    
+    return (
+        <div id="game-inner">
+            <GameCardPlaceholder card={croissant} />
+            <GameCardPlaceholder card={birthdayCake} />
+            <GameCardPlaceholder card={cherryPie} />
+            <GameCardPlaceholder card={pretzel} />
         </div>
     )
 }
@@ -23,22 +52,22 @@ export default function GameBoard() {
     };
 
     const [playerId, setPlayerId] = useState(generatePlayerId());
-    const [playerPosition, setPlayerPosition] = useState({x: 0, y: 0});
+    const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
 
     let squares: any = [];
 
     const squareNumber = 32;
-    for (let i=0; i<squareNumber; i++) {
-        squares.push(<BoardSquare key={i} id={i+1}/>);
+    for (let i = 0; i < squareNumber; i++) {
+        squares.push(<BoardSquare key={i} id={i + 1} />);
     }
 
     const playerMove = () => {
-        if (playerPosition.x < (75*11) && playerPosition.y < (75*5)) {
-            setPlayerPosition({x: playerPosition.x+75, y: 0});
-        } else if (playerPosition.y < (75*5)) {
-            setPlayerPosition({x:playerPosition.x, y: playerPosition.y+75});
+        if (playerPosition.x < (75 * 11) && playerPosition.y < (75 * 5)) {
+            setPlayerPosition({ x: playerPosition.x + 75, y: 0 });
+        } else if (playerPosition.y < (75 * 5)) {
+            setPlayerPosition({ x: playerPosition.x, y: playerPosition.y + 75 });
         } else {
-            setPlayerPosition({x:playerPosition.x - 75, y: playerPosition.y});
+            setPlayerPosition({ x: playerPosition.x - 75, y: playerPosition.y });
         }
     }
 
@@ -46,18 +75,14 @@ export default function GameBoard() {
 
     return (
         <div id="game-board">
-            <PlayerIcons playerId={playerId} playerPosition={playerPosition}/>
+            <PlayerIcons playerId={playerId} playerPosition={playerPosition} />
             {squares}
-            <div id="game-inner">
-                <GameCardPlaceholder />
-                <GameCardPlaceholder />
-                <GameCardPlaceholder />
-                <GameCardPlaceholder />
-            </div>
+            <GameInner />
             <button onClick={playerMove}>Click Me!!</button>
             <div id="player-cards-wrapper">
                 <GameCardsDeck />
             </div>
+
         </div>
     )
 };
--- src/components/pages/game/GameCards.tsx
+++ src/components/pages/game/GameCards.tsx
@@ -4,7 +4,16 @@ import bdayCakeImg from '../../../imgs/cards/birthday-cake-512x512.png';
 import cherryPieImg from '../../../imgs/cards/cherry-pie-512x512.png';
 import pretzelImg from '../../../imgs/cards/pretzel-512x512.png';
 
-export default function GameCardsDeck () {
+export function GameCardComp ({imageSrc, cardId, cardName, cardValue}: any) {  
+    return (
+        <div className='game-card' id={cardId}>
+            <img src={imageSrc} />
+            <h2>{cardName}</h2><p>{cardValue}</p>
+        </div>
+    )
+}
+
+export function GameCardsDeck () {
     class GameCard {
         name: string;
         value: number;
@@ -23,28 +32,34 @@ export default function GameCardsDeck () {
     
     let cardsDeckArray = [];
     for (let i = 0; i < 10; i++) {
-        let croissant = new GameCard("Croissant", 1, 0, i+1, croissantImg );
+        let croissant = <GameCardComp imageSrc={croissantImg} cardValue={1} cardId={i+1} cardName="Croissant" />;
+        // let croissant = new GameCard("Croissant", 1, 0, i+1, croissantImg );
         cardsDeckArray.push(croissant);
     };
     for (let i = 0; i < 2; i++) {
-        let birthdayCake = new GameCard("Birthday Cake", 10, 0, i+1, bdayCakeImg);
+        let birthdayCake = <GameCardComp imageSrc={bdayCakeImg} cardValue={10} cardId={i+1} cardName="Birthday Cake" />;
+        // let birthdayCake = new GameCard("Birthday Cake", 10, 0, i+1, bdayCakeImg);
         cardsDeckArray.push(birthdayCake);
     };
     for (let i = 0; i < 5; i++) {
-        let cherryPie = new GameCard("Cherry Pie", 5, 0, i+1, cherryPieImg );
+        let cherryPie = <GameCardComp imageSrc={cherryPieImg} cardValue={5} cardId={i+1} cardName="Cherry Pie" />
+        // let cherryPie = new GameCard("Cherry Pie", 5, 0, i+1, cherryPieImg );
         cardsDeckArray.push(cherryPie);
     };
     for (let i = 0; i < 3; i++) {
-        let pretzel = new GameCard("Pretzel", 3, 0, i+1, pretzelImg);
+       let pretzel = <GameCardComp imageSrc={pretzelImg} cardValue={3} cardId={i+1} cardName="Pretzel" />
+        // let pretzel = new GameCard("Pretzel", 3 0, i+1, pretzelImg);
         cardsDeckArray.push(pretzel);
     };
     
     console.log(cardsDeckArray);
 
     return (
-        <div className='game-card'>
-        </div>
+        <>
+            {cardsDeckArray}
+        </>
     )
+        
 }
 
 
